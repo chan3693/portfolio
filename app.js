bc4a8b96f023dc1ecc5baf717cd30a7ff04a8169
simonchan <chansk3693@gmail.com>
2024-08-15 00:12:30 -0400

    no message

---
+++ app.js
@@ -0,0 +1,15 @@
+window.onscroll = function() {
+    toggleBackToTopButton();
+  };
+  function toggleBackToTopButton() {
+    var button = document.getElementById("topBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      button.style.display = "block";
+    } else {
+      button.style.display = "none";
+    }
+  }
+  function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
