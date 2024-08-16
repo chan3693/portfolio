9865026cf4a7d4cbf3e75a9b5f4e8142c52ddc48
simonchan <chansk3693@gmail.com>
2024-08-16 17:27:16 -0400

    no message

--- app.js
+++ app.js
@@ -44,58 +44,114 @@ window.onscroll = function() {
   }
 
 // Projects control
-if (window.location.pathname.endsWith('projects.html')){
-  document.addEventListener('DOMContentLoaded', () => {
-    const projectContainer = document.getElementById('projects');
-    console.log('DOM content loaded')
-    console.log(`Total projects : ${myProjects.length}`)
-  
-    //Loop the doc
-    myProjects.forEach(project => {
-        // Create project container
-        const projectDiv = document.createElement('div');
-        projectDiv.className = 'project-container';
-  
-        // Add video container
-        const videoDiv = document.createElement('div');
-        videoDiv.className = 'video-container';
-        videoDiv.innerHTML = `<iframe src="${project.videoUrl}" width="1280" height="720" allow="autoplay"></iframe>`;
-        projectDiv.appendChild(videoDiv);
-  
-        // Add overview container
-        const overviewDiv = document.createElement('div');
-        overviewDiv.className = 'overview-container';
-        overviewDiv.innerHTML = `<h2>${project.title}</h2>`;
-  
-        // Add features if any
-        if (project.features.length > 0) {
-            const ul = document.createElement('ul');
-            project.features.forEach(feature => {
-                const li = document.createElement('li');
-                li.innerHTML = `<strong>${feature.subtitle}:</strong> ${feature.description}`;
-                ul.appendChild(li);
-            });
-            overviewDiv.appendChild(ul);
-        }
-  
-        // Add GitHub links
-        const p = document.createElement('p');
-        p.textContent = 'GitHub project:';
-        project.githubLinks.forEach( (link, index) => {
-          const a = document.createElement('a');
-          a.href = link.url;
-          a.target = '_blank';
-          a.textContent = link.name;
-          p.appendChild(a);
-  
-          if (index < project.githubLinks.length-1){
-            p.appendChild(document.createTextNode(' & '));
-          }
-        });
-        overviewDiv.appendChild(p);
-  
-        projectDiv.appendChild(overviewDiv);
-        projectContainer.appendChild(projectDiv);
+function renderProjects(projects) {
+  const projectContainer = document.getElementById('projects');
+  console.log('DOM content loaded')
+  console.log(`Total projects : ${projects.length}`)
+
+  //Loop the doc
+  projects.forEach(project => {
+    const projectDiv = document.createElement('div');
+    projectDiv.className = 'project-container';
+    // projectDiv.classList.add('project-container')
+
+    const videoDiv = document.createElement('div');
+    videoDiv.className = 'video-container';
+    // videoDiv.classList.add('video-container')
+    videoDiv.innerHTML = `<iframe src="${project.videoUrl}" width="1280" height="720" allow="autoplay"></iframe>`;
+    projectDiv.appendChild(videoDiv);
+
+    const overviewDiv = document.createElement('div');
+    overviewDiv.className = 'overview-container';
+    // overviewDiv.classList.add('overview-container')
+    overviewDiv.innerHTML = `<h2>${project.title}</h2>`;
+
+    if (project.features.length > 0) {
+      const ul = document.createElement('ul');
+      project.features.forEach(feature => {
+        const li = document.createElement('li');
+        li.innerHTML = `<strong>${feature.subtitle}:</strong> ${feature.description}`;
+        ul.appendChild(li);
+      });
+      overviewDiv.appendChild(ul);
+    }
+
+    const p = document.createElement('p');
+    p.textContent = 'GitHub project:';
+    project.githubLinks.forEach( (link, index) => {
+      const a = document.createElement('a');
+      a.href = link.url;
+      a.target = '_blank';
+      a.textContent = link.name;
+      p.appendChild(a);
+
+      if (index < project.githubLinks.length-1){
+        p.appendChild(document.createTextNode(' & '));
+      }
     });
+    overviewDiv.appendChild(p);
+
+    projectDiv.appendChild(overviewDiv);
+    projectContainer.appendChild(projectDiv);
   });
 }
+
+// index skills cards
+function renderSkillsCards(skills) {
+  const container = document.getElementById('skills-container');
+  skills.forEach(skill => {
+      const card = document.createElement('div');
+      card.classList.add('skills-card');
+
+      const title = document.createElement('h3');
+      title.textContent = skill.title;
+      card.appendChild(title);
+
+      skill.content.forEach(item => {
+          const p = document.createElement('p');
+          const strong = document.createElement('strong');
+          strong.textContent = item.category + ": ";
+          p.appendChild(strong);
+          p.appendChild(document.createTextNode(item.skills));
+          card.appendChild(p);
+      });
+
+      container.appendChild(card);
+  });
+}
+
+// resume skills list
+function renderSkillsList(skills) {
+  const container = document.getElementById('resume-skills-container');
+  
+  skills.forEach(skill => {
+      const title = document.createElement('h3');
+      title.textContent = skill.title;
+      container.appendChild(title);
+
+      const ul = document.createElement('ul');
+      
+      skill.content.forEach(item => {
+          const li = document.createElement('li');
+          const strong = document.createElement('strong');
+          strong.textContent = item.category + ": ";
+          li.appendChild(strong);
+          li.appendChild(document.createTextNode(item.skills));
+          ul.appendChild(li);
+      });
+
+      container.appendChild(ul);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const path = window.location.pathname;
+
+    if (path.endsWith('projects.html')) {
+      renderProjects(myProjects)
+    } else if (path.endsWith('index.html')) {
+      renderSkillsCards(mySkills); 
+    } else if (path.endsWith('resume.html')) {
+      renderSkillsList(mySkills)
+    }
+});
---
+++ mySkills.js
@@ -0,0 +1,28 @@
+const mySkills = [
+    {
+        title: "Programming Languages & Frameworks",
+        content: [
+            { category: "Frontend & Web/Mobile Development", 
+                skills: "Swift, Kotlin, JavaScript, HTML5, CSS3, React, Vue.js, Android, iOS, React Native, SwiftUI, UIKit" },
+            { category: "Backend & Databases", 
+                skills: "Node.js, Express.js, RESTful APIs, Python, C#, MongoDB, Firebase, SQLite" },
+            ]
+    },
+    {
+        title: "Tools & Technologies",
+        content: [
+            { category: "Version Control", 
+                skills: "Git, Sourcetree" },
+            { category: "Database Management", 
+                skills: "SQL Server, MongoDB, Firebase Firestore" },
+            { category: "Project Management", 
+                skills: "Jira, Azure Boards, Bitbucket" },
+            { category: "Development Environments", 
+                skills: "Xcode, Android Studio, Visual Studio Code, IntelliJ, HBuilderX" },
+            { category: "Other tools", 
+                skills: "Microsoft Office Suite, SharePoint, Visio, SAP, Teamcenter (PLM), Postman" },
+            ]
+    },
+]
+
+
